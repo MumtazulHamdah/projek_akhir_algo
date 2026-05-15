f5bed7878401a80c9a61114d19239fe32f35bb65
MumtazulHamdah <124250140@student.upnyk.ac.id>
2026-05-15 21:18:10 +0700

    pertama

---
+++ bioskop.cpp
@@ -0,0 +1,359 @@
+ #include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <windows.h>
+
+using namespace std;
+
+struct Film {
+    char judul[100];
+    char genre[50];
+    int harga;
+    char jam[20];
+
+    Film *next;
+};
+
+struct Riwayat {
+    char nama[100];
+    char film[100];
+
+    Riwayat *next;
+    Riwayat *prev;
+};
+
+Film *headFilm = NULL;
+Film *tailFilm = NULL;
+
+Riwayat *headRiwayat = NULL;
+Riwayat *tailRiwayat = NULL;
+
+void loading() {
+
+    cout << "\nLoading";
+
+    for(int i = 0; i < 5; i++) {
+        cout << ".";
+        Sleep(300);
+    }
+
+    cout << endl;
+}
+
+void header() {
+
+    cout << "\n";
+    cout << "============================================================\n";
+    cout << "                  BIOSKOP XXI INDONESIA                     \n";
+    cout << "============================================================\n";
+    cout << "            PESAN TIKET FILM FAVORITMU DISINI               \n";
+    cout << "============================================================\n";
+}
+
+void tambahFilm(const char *judul,
+                const char *genre,
+                int harga,
+                const char *jam) {
+
+    Film *baru = new Film;
+
+    strcpy(baru->judul, judul);
+    strcpy(baru->genre, genre);
+    baru->harga = harga;
+    strcpy(baru->jam, jam);
+
+    baru->next = NULL;
+
+    if(headFilm == NULL) {
+        headFilm = tailFilm = baru;
+    }
+    else {
+        tailFilm->next = baru;
+        tailFilm = baru;
+    }
+}
+
+void tampilFilm() {
+
+    Film *current = headFilm;
+    int no = 1;
+
+    cout << "\n====================================================================================\n";
+    cout << "| NO | JUDUL FILM                  | GENRE       | HARGA   | JAM TAYANG |\n";
+    cout << "====================================================================================\n";
+
+    while(current != NULL) {
+
+        printf("| %-2d | %-28s | %-11s | %-7d | %-11s |\n",
+               no,
+               current->judul,
+               current->genre,
+               current->harga,
+               current->jam);
+
+        current = current->next;
+        no++;
+    }
+
+    cout << "====================================================================================\n";
+}
+
+void searchingFilm() {
+
+    char cari[100];
+    int ketemu = 0;
+
+    cin.ignore();
+
+    cout << "\nMasukkan Judul Film : ";
+    cin.getline(cari, 100);
+
+    Film *current = headFilm;
+
+    while(current != NULL) {
+
+        if(stricmp(current->judul, cari) == 0) {
+
+            cout << "\n========================================\n";
+            cout << "           FILM DITEMUKAN               \n";
+            cout << "========================================\n";
+
+            cout << "Judul : " << current->judul << endl;
+            cout << "Genre : " << current->genre << endl;
+            cout << "Harga : " << current->harga << endl;
+            cout << "Jam   : " << current->jam << endl;
+
+            ketemu = 1;
+        }
+
+        current = current->next;
+    }
+
+    if(ketemu == 0) {
+        cout << "\nFilm Tidak Ditemukan!\n";
+    }
+}
+
+void sortingHarga() {
+
+    if(headFilm == NULL) {
+        return;
+    }
+
+    int tukar;
+
+    Film *ptr1;
+    Film *lptr = NULL;
+
+    do {
+
+        tukar = 0;
+        ptr1 = headFilm;
+
+        while(ptr1->next != lptr) {
+
+            if(ptr1->harga > ptr1->next->harga) {
+
+                int tempHarga;
+                char tempJudul[100];
+                char tempGenre[50];
+                char tempJam[20];
+
+                tempHarga = ptr1->harga;
+                ptr1->harga = ptr1->next->harga;
+                ptr1->next->harga = tempHarga;
+
+                strcpy(tempJudul, ptr1->judul);
+                strcpy(ptr1->judul, ptr1->next->judul);
+                strcpy(ptr1->next->judul, tempJudul);
+
+                strcpy(tempGenre, ptr1->genre);
+                strcpy(ptr1->genre, ptr1->next->genre);
+                strcpy(ptr1->next->genre, tempGenre);
+
+                strcpy(tempJam, ptr1->jam);
+                strcpy(ptr1->jam, ptr1->next->jam);
+                strcpy(ptr1->next->jam, tempJam);
+
+                tukar = 1;
+            }
+
+            ptr1 = ptr1->next;
+        }
+
+        lptr = ptr1;
+
+    } while(tukar);
+
+    cout << "\nFilm Berhasil Diurutkan Berdasarkan Harga!\n";
+}
+
+void tambahRiwayat(const char *nama,
+                   const char *film) {
+
+    Riwayat *baru = new Riwayat;
+
+    strcpy(baru->nama, nama);
+    strcpy(baru->film, film);
+
+    baru->next = NULL;
+    baru->prev = NULL;
+
+    if(headRiwayat == NULL) {
+        headRiwayat = tailRiwayat = baru;
+    }
+    else {
+
+        tailRiwayat->next = baru;
+        baru->prev = tailRiwayat;
+        tailRiwayat = baru;
+    }
+}
+
+void pesanTiket() {
+
+    char nama[100];
+    char film[100];
+
+    tampilFilm();
+
+    cin.ignore();
+
+    cout << "\nMasukkan Nama Pemesan : ";
+    cin.getline(nama, 100);
+
+    cout << "Masukkan Judul Film   : ";
+    cin.getline(film, 100);
+
+    tambahRiwayat(nama, film);
+
+    loading();
+
+    cout << "\n";
+    cout << "========================================\n";
+    cout << "             TIKET BIOSKOP              \n";
+    cout << "========================================\n";
+    cout << "Nama Pemesan : " << nama << endl;
+    cout << "Film         : " << film << endl;
+    cout << "Status       : BERHASIL\n";
+    cout << "========================================\n";
+}
+
+void tampilRiwayat() {
+
+    Riwayat *current = headRiwayat;
+    int no = 1;
+
+    cout << "\n====================================================\n";
+    cout << "              RIWAYAT PEMESANAN                     \n";
+    cout << "====================================================\n";
+
+    while(current != NULL) {
+
+        cout << "\nRiwayat " << no << endl;
+        cout << "Nama : " << current->nama << endl;
+        cout << "Film : " << current->film << endl;
+
+        current = current->next;
+        no++;
+    }
+}
+
+void simpanFile() {
+
+    FILE *fp;
+
+    fp = fopen("film.txt", "w");
+
+    Film *current = headFilm;
+
+    while(current != NULL) {
+
+        fprintf(fp,
+                "%s#%s#%d#%s\n",
+                current->judul,
+                current->genre,
+                current->harga,
+                current->jam);
+
+        current = current->next;
+    }
+
+    fclose(fp);
+
+    cout << "\nData Film Berhasil Disimpan Ke File!\n";
+}
+
+int main() {
+
+    int pilih;
+
+    system("color 0B");
+
+    tambahFilm("Spider-Man Brand New Day", "Action", 60000, "20.00");
+    tambahFilm("Danur The Last Chapter", "Horror", 45000, "21.00");
+    tambahFilm("The Odyssey", "Adventure", 55000, "18.30");
+    tambahFilm("Backrooms", "Horror", 50000, "19.00");
+    tambahFilm("Crocodile Tears", "Drama", 40000, "17.00");
+
+    do {
+
+        header();
+
+        cout << "\n";
+        cout << "1. Tampilkan Daftar Film\n";
+        cout << "2. Cari Film\n";
+        cout << "3. Sorting Harga Film\n";
+        cout << "4. Pesan Tiket\n";
+        cout << "5. Tampilkan Riwayat\n";
+        cout << "6. Simpan Data Ke File\n";
+        cout << "0. Keluar\n";
+
+        cout << "\nPilih Menu : ";
+        cin >> pilih;
+
+        switch(pilih) {
+
+            case 1:
+                tampilFilm();
+                break;
+
+            case 2:
+                searchingFilm();
+                break;
+
+            case 3:
+                sortingHarga();
+                tampilFilm();
+                break;
+
+            case 4:
+                pesanTiket();
+                break;
+
+            case 5:
+                tampilRiwayat();
+                break;
+
+            case 6:
+                simpanFile();
+                break;
+
+            case 0:
+                cout << "\n========================================\n";
+                cout << "        TERIMA KASIH TELAH DATANG       \n";
+                cout << "========================================\n";
+                break;
+
+            default:
+                cout << "\nMenu Tidak Tersedia!\n";
+        }
+
+        cout << endl;
+        system("pause");
+        system("cls");
+
+    } while(pilih != 0);
+
+    return 0;
+}
